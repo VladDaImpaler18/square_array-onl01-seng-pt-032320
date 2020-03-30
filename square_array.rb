ce544649c2b77c5633abfbeb5bdd5e25bddfc333
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-30 03:19:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
-def square_array(array)
-  newArray=[]
-  array.each{ |number|
-  number*=number
-    newArray << number
-  }
-  return newArray
-end
+#def square_array(array)
+#  newArray=[]
+#  array.each{|number|
+#  number*=number
+#    newArray << number
+#  }
+#  return newArray
+#end
